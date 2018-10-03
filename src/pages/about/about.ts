77d364ddb0aef720439169c2ded088a4cc0a7260
gbrielle <zacharyweller@Zacharys-MacBook-Pro.local>
2018-10-02 23:09:10 -0500

    photo gallery, photo currentImage

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
-
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
@@ -9,22 +8,22 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 export class AboutPage {
   currentImage: any;
   constructor(public navCtrl: NavController, private camera: Camera) {
-
-  }
-
 }
-takePicture() {
-  const options: CameraOptions = {
-    quality: 100,
-    destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
+
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+  
+    this.camera.getPicture(options).then((imageData) => {
+      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+     console.log("Camera issue:" + err);
+    });
   }
 
-  this.camera.getPicture(options).then((imageData) => {
-    this.currentImage = 'data:image/jpeg;base64,' + imageData;
-  }, (err) => {
-   // Handle error
-   console.log("Camera issue:" + err);
-  });
 }
